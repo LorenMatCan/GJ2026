6155450bc6326e4718405320e24055b14fa4037d
LorenMatCan <saramatutec@ciencias.unam.mx>
2026-01-30 03:39:51 -0600

    Se añaden prefabs musica

---
+++ Pesadillas/Assets/Scripts/Musica/SoundManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager soundM;
+    [SerializeField] Image sonido; 
+    [SerializeField] Image sinSonido;
+
+    public bool silencio;
+
+    private void Awake()
+    {
+        if (soundM != null && soundM  != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        soundM = this; 
+        DontDestroyOnLoad(this); 
+        silencio = !BGM.BGMMenu.EstadoMusica();
+        ActualizarBoton();  
+    }
+
+    /// Al presionar el boton, dependiendo del estado actual se activan o desactivan los sonidos 
+    public void OnButtonPress()
+    {
+        if(!silencio)
+        {
+            silencio  = true;
+            SFX.SFXMenu.PararSFX(); 
+            BGM.BGMMenu.PararMusica(); 
+        }
+        else
+        {
+            silencio = false;
+            SFX.SFXMenu.ContinuarSFX(); 
+            BGM.BGMMenu.ContinuarMusica();
+        }
+        ActualizarBoton(); 
+    } 
+
+    //Modificamos la iamgen del boton dependiendo del estado actual
+    private void ActualizarBoton()
+    {
+        if(!silencio)
+        {
+            sonido.gameObject.SetActive(true);
+            sinSonido.gameObject.SetActive(false);
+        }
+        else
+        {
+            sonido.gameObject.SetActive(false);
+            sinSonido.gameObject.SetActive(true);
+        }
+    }
+    
+}
---
+++ Pesadillas/Assets/Scripts/PlayerSoundController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSoundController : MonoBehaviour
+{
+    public AudioSource source; 
+    public AudioClip musica;
+    public PlayerController playerController;
+    private bool activo=false ;
+    private IEnumerator tiempo; 
+
+    void Start()
+    {
+        playerController.Caminar += InicirPasos; 
+        playerController.Parar += PararPasos; 
+    }
+
+    private void InicirPasos()
+    {
+        if(!activo)
+        {
+            activo=true;
+            //source.loop = true; 
+            source.clip = musica; 
+            //source.Play();
+            tiempo = PlayAudioWithDelay(); 
+            StartCoroutine(tiempo);
+        }
+    }
+
+    
+
+    private void PararPasos()
+    {
+        StopCoroutine(tiempo);
+        source.Pause(); 
+        activo =false;
+    }
+
+    IEnumerator PlayAudioWithDelay()
+    {
+        while (true) // Loop indefinitely
+        {
+            source.Play();
+            // Wait for the duration of the clip
+            yield return new WaitForSeconds(source.clip.length);
+
+            // Wait for the specified delay before the next loop
+            yield return new WaitForSeconds(0.125f);
+        }
+    }
+
+    void OnDestroy()
+    {
+        playerController.Caminar -= InicirPasos; 
+        playerController.Parar -= PararPasos; 
+    }
+
+}
